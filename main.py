b2e4379928ea44aace994c55b4fc86f966a99a38
avivwachman <68813905+avivwachman@users.noreply.github.com>
2020-10-25 17:26:36 +0200

    By file input

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 """ Vending Machine,   Aviv Wachman"""
-import sys
+
+
+def round_only_up(floater, up_by):
+    rounded_float = round(floater, up_by)
+    if rounded_float < floater:
+        return floater
+    else:
+        return rounded_float
+
+
+def open_file(fname):
+    input_lst = list()
+    if len(fname) < 1:
+        fname = 'input.txt'
+    fh = open(fname)
+    for line in fh:
+        line = line.rstrip().split(" ")
+        line_input = (line[0], line[1])
+        input_lst.append(line_input)
+    return input_lst
 
 
 def init_machine():
-    num_mach = {
+    bev_dict = {
         "1": "Ness Coffee",
         "2": "Coffee Black",
         "3": "Tea",
@@ -12,39 +31,39 @@ def init_machine():
         "6": "Coca-Cola",
         "7": "Orange Juice"
     }
-    money_mach = (2.4, 2.1, 1.6, 2.5, 3.1, 3.3, 3.2)
-    return num_mach, money_mach
+    money_tuple = (2.4, 2.1, 1.6, 2.5, 3.1, 3.3, 3.2)
+    return bev_dict, money_tuple
 
 
-def print_menu(num_mach, money_mach):
+def print_menu(bev_dict, money_tuple):
     print("""         MENU\n        ~~~~~~""")
-    for bev_num in num_mach:
-        beverage_name = num_mach[bev_num]
-        print(bev_num + ".   ", beverage_name, money_mach[int(bev_num) - 1])
+    for bev_num in bev_dict:
+        beverage_name = bev_dict[bev_num]
+        print(bev_num + ".   ", beverage_name, money_tuple[int(bev_num) - 1])
     print()
 
 
-def choose(num_mach, money_mach):
-    bev = input("Choose by number -->")
-    print(f"You chose {num_mach[bev]}")
-    nis_to_get = money_mach[int(bev) - 1]
+def choose(bev, bev_dict, money_tuple):
+    print(f"You entered {bev}")
+    if int(bev) not in range(1, 8):
+        print("ERROR: no beverage with that number")
+        return "NO"
+    print(f"You chose {bev_dict[bev]}")
+    nis_to_get = money_tuple[int(bev) - 1]
     print(f"Enter {nis_to_get} NIS -->", end='')
     return nis_to_get
 
 
-def enough_nis():
-    print("Thank you for using my beverage machine")
-    sys.exit()
-
-
-def give_back(nis_to_get):
-    nis_entered = input()
+def give_back(nis_entered, nis_to_get):
+    print(f"You entered {nis_entered}")
     excess = round(float(nis_entered) - float(nis_to_get), 2)
     if excess == 0:
-        enough_nis()
+        print("Thank you for using my beverage machine")
+        return 0
     if excess < 0:
         print("Not Enough Money")
-        sys.exit()
+        print("You are missing", abs(excess))
+        return ""
     print("Your excess is:", excess)
     return excess
 
@@ -54,14 +73,28 @@ def print_coins(excess):
     for coin in coins:
         if coin < excess:
             num_of_coins = int(excess / coin)
-            print(num_of_coins, "coins of", coin)
-            excess = round(excess - num_of_coins * coin, 5)
-    enough_nis()
+            print(num_of_coins, "coins of", coin, "NIS")
+            excess = round_only_up(excess - num_of_coins * coin, 5)
+    print("Thank you for using my beverage machine")
+
+
+def main(bev_dict, entered_money):
+    NIS_to_get = choose(bev_dict, BEV_DICT, MONEY_TUPLE)
+    if isinstance(NIS_to_get, str):
+        return
+    EXCESS = give_back(entered_money, NIS_to_get)
+    if isinstance(EXCESS, str):
+        return
+    elif EXCESS != 0:
+        print_coins(EXCESS)
 
 
 if __name__ == '__main__':
-    NUM_mach, MONEY_mach = init_machine()
-    print_menu(NUM_mach, MONEY_mach)
-    NIS_to_get = choose(NUM_mach, MONEY_mach)
-    EXCESS = give_back(NIS_to_get)
-    print_coins(EXCESS)
+    BEV_DICT, MONEY_TUPLE = init_machine()
+    print_menu(BEV_DICT, MONEY_TUPLE)
+    INPUT_LST = open_file("")
+    for i in INPUT_LST:
+        main(i[0], i[1])
+        print()
+
+
