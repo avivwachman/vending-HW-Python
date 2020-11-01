a12831c7505eaab797e2c40d08ab83061d20ca58
avivwachman <68813905+avivwachman@users.noreply.github.com>
2020-11-01 11:16:39 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 
 def round_only_up(floater, up_by):
+    """Gets a float to round, by how much to round and returns the rounded float only if it's it's smaller"""
     rounded_float = round(floater, up_by)
     if rounded_float < floater:
         return floater
@@ -10,9 +11,10 @@ def round_only_up(floater, up_by):
 
 
 def open_file(fname):
+    """Gets a String of file location, opens it and return a list of tuples with the input"""
     input_lst = list()
     if len(fname) < 1:
-        fname = 'input.txt'
+        fname = '/Users/avivwach/Desktop/input.txt'
     fh = open(fname)
     for line in fh:
         line = line.rstrip().split(" ")
@@ -22,6 +24,7 @@ def open_file(fname):
 
 
 def init_machine():
+    """Creates a dict of the drinks, tuple of the prices and returns both"""
     bev_dict = {
         "1": "Ness Coffee",
         "2": "Coffee Black",
@@ -36,6 +39,7 @@ def init_machine():
 
 
 def print_menu(bev_dict, money_tuple):
+    """Gets a dict and tuple- prints the menu: number and drink (form dict) , price (from tuple)"""
     print("""         MENU\n        ~~~~~~""")
     for bev_num in bev_dict:
         beverage_name = bev_dict[bev_num]
@@ -44,6 +48,8 @@ def print_menu(bev_dict, money_tuple):
 
 
 def choose(bev, bev_dict, money_tuple):
+    """ Get's the chosen beverage and the a dict and tuple with the prices and name of each drink.
+    Returns NIS to pay (if a valid number was chosen) """
     print(f"You entered {bev}")
     if int(bev) not in range(1, 8):
         print("ERROR: no beverage with that number")
@@ -54,8 +60,27 @@ def choose(bev, bev_dict, money_tuple):
     return nis_to_get
 
 
+def check_num(money):
+    """Gets a float and checks how many after it goes.
+     If goes more than once after the for, returns true. Else returns true."""
+    money = str(money)
+    money = money.split('.')
+    if len(money) == 1:
+        return True
+    after_dot_len = len(money[1])
+    if after_dot_len > 1:
+        print("ERROR- entered coin not in use")
+        return False
+    return True
+
+
 def give_back(nis_entered, nis_to_get):
-    print(f"You entered {nis_entered}")
+    """Get's the paid amount and the payment required. Returns the excess or the amount needed to add or
+     thanks you for using the vending machine"""
+    print(f"You entered {nis_entered} NIS")
+    if not check_num(nis_entered):
+        print(f"You got back {nis_entered} NIS")
+        return ""
     excess = round(float(nis_entered) - float(nis_to_get), 2)
     if excess == 0:
         print("Thank you for using my beverage machine")
@@ -69,6 +94,7 @@ def give_back(nis_entered, nis_to_get):
 
 
 def print_coins(excess):
+    """Gets the excess and prints the least amount of coins to return"""
     coins = (20, 10, 5, 2, 1, 0.5, 0.1)
     for coin in coins:
         if coin < excess:
@@ -78,23 +104,24 @@ def print_coins(excess):
     print("Thank you for using my beverage machine")
 
 
-def main(bev_dict, entered_money):
-    NIS_to_get = choose(bev_dict, BEV_DICT, MONEY_TUPLE)
-    if isinstance(NIS_to_get, str):
+def main(chosen_bev, entered_money, bev_dict, money_tuple):
+    """Gets the chosen bev index(int), money paid(float), dict of bevs and tuple of prices
+     return excess or needed money to play or thanks you for using the vending machine"""
+    nis_to_get = choose(chosen_bev, bev_dict, money_tuple)
+    if isinstance(nis_to_get, str):
         return
-    EXCESS = give_back(entered_money, NIS_to_get)
-    if isinstance(EXCESS, str):
+    excess = give_back(entered_money, nis_to_get)
+    if isinstance(excess, str):
         return
-    elif EXCESS != 0:
-        print_coins(EXCESS)
+    elif excess != 0:
+        print_coins(excess)
 
 
 if __name__ == '__main__':
+    """Prints the menu, goes through the list and runs 'main' """
     BEV_DICT, MONEY_TUPLE = init_machine()
     print_menu(BEV_DICT, MONEY_TUPLE)
     INPUT_LST = open_file("")
     for i in INPUT_LST:
-        main(i[0], i[1])
+        main(i[0], i[1], BEV_DICT, MONEY_TUPLE)
         print()
-
-
