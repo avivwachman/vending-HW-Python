7c5b033acb8dbc7440f9bb3277bd08cf1494d8ad
avivwachman <68813905+avivwachman@users.noreply.github.com>
2020-11-06 21:53:06 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -10,30 +10,10 @@ def round_only_up(floater, up_by):
         return rounded_float
 
 
-def open_file(fname):
-    """Gets a String of file location, opens it and return a list of tuples with the input"""
-    input_lst = list()
-    if len(fname) < 1:
-        fname = '/Users/avivwach/Desktop/input.txt'
-    fh = open(fname)
-    for line in fh:
-        line = line.rstrip().split(" ")
-        line_input = (line[0], line[1])
-        input_lst.append(line_input)
-    return input_lst
-
-
 def init_machine():
     """Creates a dict of the drinks, tuple of the prices and returns both"""
-    bev_dict = {
-        "1": "Ness Coffee",
-        "2": "Coffee Black",
-        "3": "Tea",
-        "4": "Hot Chocolate",
-        "5": "Soup",
-        "6": "Coca-Cola",
-        "7": "Orange Juice"
-    }
+    bev_dict = {"1": "Ness Coffee", "2": "Coffee Black", "3": "Tea", "4": "Hot Chocolate", "5": "Soup",
+                "6": "Coca-Cola", "7": "Orange Juice"}
     money_tuple = (2.4, 2.1, 1.6, 2.5, 3.1, 3.3, 3.2)
     return bev_dict, money_tuple
 
@@ -48,12 +28,12 @@ def print_menu(bev_dict, money_tuple):
 
 
 def choose(bev, bev_dict, money_tuple):
-    """ Get's the chosen beverage and the a dict and tuple with the prices and name of each drink.
+    """ Get's the chosen beverage, a dict and tuple with the prices and name of each drink.
     Returns NIS to pay (if a valid number was chosen) """
     print(f"You entered {bev}")
     if int(bev) not in range(1, 8):
         print("ERROR: no beverage with that number")
-        return "NO"
+        return  # return None
     print(f"You chose {bev_dict[bev]}")
     nis_to_get = money_tuple[int(bev) - 1]
     print(f"Enter {nis_to_get} NIS -->", end='')
@@ -62,11 +42,11 @@ def choose(bev, bev_dict, money_tuple):
 
 def check_num(money):
     """Gets a float and checks how many after it goes.
-     If goes more than once after the for, returns true. Else returns true."""
+     If goes more than once after the dot, returns false. Else returns true."""
     money = str(money)
     money = money.split('.')
     if len(money) == 1:
-        return True
+        return False
     after_dot_len = len(money[1])
     if after_dot_len > 1:
         print("ERROR- entered coin not in use")
@@ -80,7 +60,7 @@ def give_back(nis_entered, nis_to_get):
     print(f"You entered {nis_entered} NIS")
     if not check_num(nis_entered):
         print(f"You got back {nis_entered} NIS")
-        return ""
+        return  # return None
     excess = round(float(nis_entered) - float(nis_to_get), 2)
     if excess == 0:
         print("Thank you for using my beverage machine")
@@ -88,7 +68,7 @@ def give_back(nis_entered, nis_to_get):
     if excess < 0:
         print("Not Enough Money")
         print("You are missing", abs(excess))
-        return ""
+        return  # return None
     print("Your excess is:", excess)
     return excess
 
@@ -108,20 +88,28 @@ def main(chosen_bev, entered_money, bev_dict, money_tuple):
     """Gets the chosen bev index(int), money paid(float), dict of bevs and tuple of prices
      return excess or needed money to play or thanks you for using the vending machine"""
     nis_to_get = choose(chosen_bev, bev_dict, money_tuple)
-    if isinstance(nis_to_get, str):
+    if nis_to_get is None:
         return
     excess = give_back(entered_money, nis_to_get)
-    if isinstance(excess, str):
+    if excess is None:
         return
     elif excess != 0:
         print_coins(excess)
 
 
+def open_file(fname, bev_dict, money_tuple):
+    """Gets a String of file location, opens it and calls main with the chosen beverage and money entered"""
+    if len(fname) < 1:
+        fname = '/Users/avivwach/Desktop/text.txt'
+    fh = open(fname)
+    for line in fh:
+        line = line.rstrip().split(" ")
+        main(line[0], line[1], bev_dict, money_tuple)
+        print()
+
+
 if __name__ == '__main__':
-    """Prints the menu, goes through the list and runs 'main' """
+    """Prints the menu, calls open_file """
     BEV_DICT, MONEY_TUPLE = init_machine()
     print_menu(BEV_DICT, MONEY_TUPLE)
-    INPUT_LST = open_file("")
-    for i in INPUT_LST:
-        main(i[0], i[1], BEV_DICT, MONEY_TUPLE)
-        print()
+    open_file("", BEV_DICT, MONEY_TUPLE)
